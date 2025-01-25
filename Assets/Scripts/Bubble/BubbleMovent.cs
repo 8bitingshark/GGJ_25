292dbf0d15aa85a591590e44ba6728d19b264540
MSI-MICKAEL\ordin <ordinemickael99@gmail.com>
2025-01-25 03:43:35 +0100

    bubble modular spawn part 2

---
+++ Assets/Scripts/Bubble/BubbleMovent.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * to fix the max amount of velocity
+ * 
+ */
+public class BubbleMovent : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float _maxSpeed = 1.0f;
+    [SerializeField] private float _forceAmount = 0.01f;
+    [SerializeField] private float _relativeJointForce = 6.3f;
+    [SerializeField] private float _forceAmountWithPlayer = 5f;
+    [SerializeField] private float _secondsToWaitWhenPLayerEnter = 0.01f;
+
+    [SerializeField] private LayerMask whatIsPlayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //2 frames
+    void FixedUpdate()
+    {
+        Move();
+        Debug.Log(rb.linearVelocityY);
+    }
+    
+    
+    void Move()
+    {
+        if(rb.linearVelocityY < _maxSpeed)
+            rb.AddForce(new Vector2(0, _forceAmount), ForceMode2D.Impulse);
+        
+    }
+
+    IEnumerator timerContact()
+    {
+        yield return new WaitForSeconds(_secondsToWaitWhenPLayerEnter);
+        gameObject.GetComponent<RelativeJoint2D>().maxForce = 6.3f;//valore fisso per far cadere
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        if ((whatIsPlayer.value & (1 << other.gameObject.layer)) > 0)
+        {
+            //aggiungi forza all'insù
+            gameObject.GetComponent<RelativeJoint2D>().enabled = true;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+            // rendere statica la bubble
+            StartCoroutine(timerContact());
+        }
+        else
+        {
+            Debug.Log(other.gameObject.name + " non è in uno dei layer specificati.");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        //aggiungi effetto che va giù la bolla di pochi secondi come se la stessimo leggermente spingendo giù
+        StopCoroutine(timerContact());
+        gameObject.GetComponent<RelativeJoint2D>().enabled = false;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        gameObject.GetComponent<RelativeJoint2D>().maxForce = 40;
+    }
+}
---
+++ Assets/Scripts/Bubble/BubbleSpawner.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject bubblePrefab;
+    [SerializeField] private GameObject startPosition;
+    [SerializeField] private GameObject endPosition;
+    [SerializeField] private float spawnInterval = 2.5f;
+
+    [SerializeField] private Vector2 startVector;
+    [SerializeField] private Vector2 endVector;
+    [SerializeField] private List<Vector2> VectorPositionBubbles = new List<Vector2>();
+    private float cellSize;
+    private float timer = 0;
+    private bool flag = false;
+    [SerializeField] private int cellsAmount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        startVector = startPosition.transform.position;
+        endVector = endPosition.transform.position;
+    }
+
+    void Start()
+    {
+        cellSize = (endVector.x - startVector.x) / cellsAmount;
+        SetUpvectorSpawn();
+    }
+
+    void SetUpvectorSpawn()
+    {
+        for (int i = 0; i < cellsAmount; i++)
+        {
+            VectorPositionBubbles.Add(startVector);
+            startVector.x += cellSize;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            spawnBubbleAction(flag);
+            timer = 0;
+            flag = !flag;
+        }
+    }
+
+    void spawnBubbleAction(bool evenCycle)
+    {
+        for (int i = 0; i < VectorPositionBubbles.Count; i++)
+        {
+            if (evenCycle)
+            {
+                if (i % 2 == 0)
+                {
+                    GameObject bubble = Instantiate(bubblePrefab);
+                    bubble.transform.position = VectorPositionBubbles[i];
+                }
+            }
+            else
+            {
+                if (i % 2 != 0)
+                {
+                    GameObject bubble = Instantiate(bubblePrefab);
+                    bubble.transform.position = VectorPositionBubbles[i];
+                }
+            }
+        }
+    }
+}
