dbd0067afc51ab1bc1f01740f6f08cfc9a4a9318
MSI-MICKAEL\ordin <ordinemickael99@gmail.com>
2025-01-26 04:41:14 +0100

    added level design By Tomez

--- Assets/Scripts/Platform/MovingPlatformScript.cs
+++ Assets/Scripts/Platform/MovingPlatformScript.cs
@@ -5,23 +5,12 @@ using UnityEngine.UIElements;
 public class MovingPlatformScript : MonoBehaviour
 {
     [Header("Punti di riferimento")]
-    public Transform pointA; // Primo punto
-    public Transform pointB; // Secondo punto
     public Vector2 posA;
     public Vector2 posB;
 
     [Header("Parametri movimento")]
     public float speed = 0.1f; // Velocità della piattaforma
     private Transform target; // Punto di destinazione attuale
-    
-    
-    private void Start()
-    {
-        // Inizia muovendosi verso il primo punto
-        target = pointB;
-        posA = pointA.position;
-        posB = pointB.position;
-    }
 
     private void Update()
     {
